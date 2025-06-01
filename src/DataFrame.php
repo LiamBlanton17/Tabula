a1be898b05b6d4367ece56680c83d587e2e505f8
LiamBlanton17 <liamblanton@gmail.com>
2025-05-31 22:30:35 -0700

    Added data field to DataFrame

--- src/DataFrame.php
+++ src/DataFrame.php
@@ -5,8 +5,13 @@
  */
 class DataFrame {
 
+    /**
+     * @var array the associative array of data
+     */
+    private array $data;
+
     public function __construct(array $data) {
-        
+        $this->data = $data;
     }
 
 }
